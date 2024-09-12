ae57e70ec8b1a07794a91d0d0d9a7ad4442f8f56
fuad.malaeb <fuad.malaeb@auleseebe.upc.edu>
2024-09-12 12:03:31 +0200

    ...

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,14 @@
 #Codigo para problema 3
+
+x <- c(-8,5,2,-8,9,5,2,-3,1,-1,4,-4,9,3,-9,7,0,-7,8,-4,1,7,-6,4,5,-9,-2,-8,5,-5)
+#c es concatenar
+
+#calcular el sumatorio
+sum(x)
+
+x[c(1, 2, 3)]
+x[-c(1,2,3)]
+y = x[-c(21,2,26)]
+
+sum(exp(x))- sum(exp(y))
+y[c(19,24)]
